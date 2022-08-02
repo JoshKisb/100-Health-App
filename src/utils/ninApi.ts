a0e558a41c32eeb460e60088282a02fb1827f394
Josh Kisb <joshuakisb@gmail.com>
2022-08-02 07:26:47 +0300

    retry fetch nin api on timeout

--- src/utils/ninApi.ts
+++ src/utils/ninApi.ts
@@ -1,47 +1,59 @@
+import originalFetch from "isomorphic-unfetch";
+import fetchBuilder from "fetch-retry-ts";
+
 const baseURL = "https://hmis-dev.health.go.ug/db-api/api/v2";
-const defaultToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhIjoiJDJiJDEyJGMuMFFCZ1RRbVI5ZE1GcFcyWXhiZS5NVG5JdmZ4M2s0Qi40bjJiYzdaY2hpWFcxY1BuQk8yIiwidXNlciI6ImFkbWluMiJ9.nRYsIQpcXK4yQlvKlpZkvW1XnNr21MXf9puzgCtpmfA";
+const defaultToken =
+	"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhIjoiJDJiJDEyJGMuMFFCZ1RRbVI5ZE1GcFcyWXhiZS5NVG5JdmZ4M2s0Qi40bjJiYzdaY2hpWFcxY1BuQk8yIiwidXNlciI6ImFkbWluMiJ9.nRYsIQpcXK4yQlvKlpZkvW1XnNr21MXf9puzgCtpmfA";
 
 let token;
 
+const options = {
+	retries: 3,
+	retryDelay: 1000,
+	retryOn: [419, 503, 504],
+};
+
+const fetch = fetchBuilder(originalFetch);
+
 export const fetchNINToken = async (engine) => {
 	// check exists
-	const res = await engine.link.fetch('/api/dataStore')
+	const res = await engine.link.fetch("/api/dataStore");
 	console.log(res);
 	if (!res.includes("NINtoken")) {
-      // Create the name space
-      engine.link.fetch(`/api/dataStore/NINtoken/NINtoken`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({token: defaultToken}),
-      });
+		// Create the name space
+		engine.link.fetch(`/api/dataStore/NINtoken/NINtoken`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ token: defaultToken }),
+		});
 		token = defaultToken;
 	} else {
 		const res = await engine.link.fetch(`/api/dataStore/NINtoken/NINtoken`);
 		token = res.token;
-	}		
-}
+	}
+};
 
-
-export const getNINPerson = nin => {
+export const getNINPerson = (nin) => {
 	return fetch(`${baseURL}/getPerson`, {
-		method: 'POST',
+		method: "POST",
+		retries: 3,
+		retryDelay: 3000,
+		retryOn: [419, 503, 504],
 		body: JSON.stringify({
-			"nationalId": nin,
-			"token": token
+			nationalId: nin,
+			token: token,
 		}),
-	})
-	.then(response => response.json())
-}
+	}).then((response) => response.json());
+};
 
-export const getNINPlaceOfBirth = nin => {
+export const getNINPlaceOfBirth = (nin) => {
 	return fetch(`${baseURL}/getPlaceOfBirth`, {
-		method: 'POST',
+		method: "POST",
 		body: JSON.stringify({
-			"nationalId": nin,
-			"token": token
+			nationalId: nin,
+			token: token,
 		}),
-	})
-	.then(response => response.json())
-}
+	}).then((response) => response.json());
+};
