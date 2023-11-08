a51657917035a0a5140ff130640a06c0d1375d07
nomisrmugisa <nomisrmugisa@gmail.com>
2023-11-09 01:31:53 +0300

    baby age mappings

--- src/App.tsx
+++ src/App.tsx
@@ -75,16 +75,16 @@ export const App = observer(() => {
 
 	return (
 		<StoreContext.Provider value={store}>
-			{/* <HeaderBar
-				// appName={" Medical Certificate of Cause of Death"}
-				// style={{
-				// 	left: 0,
-				// 	position: "fixed",
-				// 	top: 0,
-				// 	width: "100%",
-				// 	zIndex: 1000,
-				// }}
-			/> */}
+			 <HeaderBar
+				appName={" Medical Certificate of Cause of Death"}
+				style={{
+					left: 0,
+					position: "fixed",
+					top: 0,
+					width: "100%",
+					zIndex: 1000,
+				}}
+			/>
 			{store.currentPage === "4" ? (
 				<div className="p-2">
 					<ApiConfigPage />
