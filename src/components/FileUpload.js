56fed1eb75e7a1be243a6244377a9e609c70a023
nomisrmugisa <nomisrmugisa@gmail.com>
2024-01-25 13:56:41 +0300

    fixes

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,24 +1,34 @@
 import React, { useState } from 'react';
 import * as XLSX from 'xlsx';
-import {parseISO, format} from "date-fns";
+import './FileUpload.css';
 
 function ExcelToJsonConverter() {
     const [file, setFile] = useState(null);
     const [jsonData, setJsonData] = useState('');
 
-    // const formatToYYYYMMDD = (dateString) => {
-    //     try {
-    //         console.log(dateString)
-    //         // Parse the date string using date-fns
-    //         const parsedDate = parseISO(dateString);
-    //
-    //         // Format the parsed date to YYYY-MM-DD
-    //         return format(parsedDate, 'yyyy-MM-dd');
-    //     } catch (error) {
-    //         console.error('Error parsing date:', error);
-    //         return 'Invalid Date';
-    //     }
-    // };
+    // Function to format the Excel date serial number
+    const formatDateFromExcelSerial = (excelSerial) => {
+        // Convert the Excel serial number to milliseconds
+        const milliseconds = (excelSerial - 25569) * 24 * 60 * 60 * 1000;
+
+        // Check if milliseconds is a valid number
+        if (isNaN(milliseconds) || !isFinite(milliseconds)) {
+            console.error('Invalid milliseconds:', milliseconds);
+            return 'Invalid Date';
+        }
+
+        // Calculate the date from the base date (January 1, 1970)
+        const date = new Date(milliseconds);
+
+
+        // Check if the Date object is valid
+        if (isNaN(date.getTime())) {
+            console.error('Invalid Date object:', date);
+            return 'Invalid Date';
+        }
+
+        return date.toISOString();
+    };
 
     const handleConvert = () => {
         if (file) {
@@ -29,48 +39,49 @@ function ExcelToJsonConverter() {
                 const sheetName = workbook.SheetNames[0];
                 const worksheet = workbook.Sheets[sheetName];
                 const json = XLSX.utils.sheet_to_json(worksheet);
-                console.log(json)
+                // console.log(json)
                 // setJsonData(JSON.stringify(json, null, 2));
 
                 const convertedJsonData = {
                     trackedEntityInstances: json.map(row => ({
-                        trackedEntityInstance: row.trackedEntityInstance,
+                        trackedEntityInstance: row.TrackedEntityInstances,
                         orgUnit: row.OrgUIDs,
                         trackedEntityType: "T5DWDr5Swce",
-                        attributes: Object.keys(row).slice(3, 14).map((key) => ({
+                        attributes: Object.keys(row).slice(4, 12).map((key) => ({
                             attribute: key,
                             value: row[key]
                         })),
                         enrollments: [
                             {
-                                orgUnit: row.orgUnitId,
-                                program: row.programId,
-                                enrollmentDate: row.enrollmentDate,
-                                incidentDate: row.incidentDate,
+                                orgUnit: row.OrgUIDs,
+                                program: "h0iSBI3xoS6",
+                                enrollmentDate: formatDateFromExcelSerial(row.EnrollmentDate),
+                                incidentDate: formatDateFromExcelSerial(row.IncidentDate),
                                 status: "ACTIVE",
-                                attributes: Object.keys(row).slice(3, 14).map((key) => ({
+                                attributes: Object.keys(row).slice(4, 12).map((key) => ({
                                     attribute: key,
                                     value: row[key]
                                 })),
                                 events: [
                                     {
-                                        program: row.programId,
-                                        orgUnit: row.orgUnitId,
-                                        eventDate: "YYYY-MM-DD",
+                                        program: "h0iSBI3xoS6",
+                                        orgUnit: row.OrgUIDs,
+                                        eventDate: formatDateFromExcelSerial(row.uxHOAUsyDKz),
                                         status: "COMPLETED",
-                                        programStage: row.programStageId,
-                                        dataValues: Object.keys(row).slice(15, 29).map((key) => ({
+                                        programStage: "nknoeOj6dLq",
+                                        dataValues: Object.keys(row).slice(12, 24).map((key) => ({
                                             dataElement: key,
-                                            value: row[key]
+                                            // value: row[key]
+                                            value: key === 'uxHOAUsyDKz' || key === 'sKrn2rY6l0w' ? formatDateFromExcelSerial(row[key]) : row[key]
                                         }))
                                     },
                                     {
-                                        program: row.programId,
-                                        orgUnit: row.orgUnitId,
-                                        eventDate: "YYYY-MM-DD",
+                                        program: "h0iSBI3xoS6",
+                                        orgUnit: row.OrgUIDs,
+                                        eventDate: formatDateFromExcelSerial(row.eventTwoDate),
                                         status: "COMPLETED",
-                                        programStage: row.programStageId,
-                                        dataValues: Object.keys(row).slice(15, 29).map((key) => ({
+                                        programStage: "s1kg8duJ8U1",
+                                        dataValues: Object.keys(row).slice(25, 29).map((key) => ({
                                             dataElement: key,
                                             value: row[key]
                                         }))
@@ -112,9 +123,10 @@ function ExcelToJsonConverter() {
     };
 
     return (
-        <div>
+        <div className="form-container">
+            <h1> Upload Excel File </h1>
             <input type="file" accept=".xls,.xlsx" onChange={e => setFile(e.target.files[0])} />
-            <button onClick={handleConvert}>Upload</button>
+            <button className="upload-btn" onClick={handleConvert}>Upload</button>
             <pre>{jsonData}</pre>
         </div>
     );
