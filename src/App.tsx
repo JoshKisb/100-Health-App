bf5d2f4669331e969c580702b44a20afd45373c7
nomisrmugisa <nomisrmugisa@gmail.com>
2024-02-01 14:25:19 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react";
 import React from "react";
+import { HeaderBar } from "@dhis2/ui-widgets";
 import ExcelToJsonConverter from "./components/FileUpload";
 
 
@@ -8,6 +9,16 @@ export const App = observer(() => {
 
   return (
       <>
+          <HeaderBar
+              appName={" File uploader"}
+              style={{
+                  left: 0,
+                  position: "fixed",
+                  top: 0,
+                  width: "100%",
+                  zIndex: 1000,
+              }}
+          />
           <ExcelToJsonConverter/>
       </>
 
