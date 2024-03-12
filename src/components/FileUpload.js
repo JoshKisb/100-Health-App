99159833165b56669c1797d60bf010bdecb18f32
nomisrmugisa <nomisrmugisa@gmail.com>
2024-03-12 12:48:27 +0300

    fix update event two

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -65,12 +65,8 @@ function ExcelToJsonConverter() {
                 for (const row of json) {
                     const id = row.TrackedEntityInstances; //  'ID'  column name
                     const exists = await checkIdExistence(id);
-                    if (exists) {
-                        // Generate dataValues array dynamically based on column names
-                        // const eventOne = columnNames.map(columnName => ({
-                        //     dataElement: columnName,
-                        //     value: columnName === 'uxHOAUsyDKz' || columnName === 'sKrn2rY6l0w' || columnName === 'ArUaftNaqGt' || columnName === 'WnHQ3OUmUal' ? formatDateFromExcelSerial(row[columnName]) : row[columnName]
-                        // }));
+                    // console.log(exists.eventId)
+                    if (exists.found) {
 
                         const eventTwo = eventTwoColumnNames.map(columnName => ({
                             dataElement: columnName,
@@ -78,39 +74,21 @@ function ExcelToJsonConverter() {
                         })).filter(row => row.value === true);
                         console.log("true values ", eventTwo)
 
+                        // update data
+                        for (const event of eventTwo) {
+                            const updatedData = {
+                                dataValues: [{
+                                    dataElement: event.dataElement,
+                                    value: event.value
+                                }]
+                            };
 
-                        // If ID exists, update record
-                        const updatedData ={
-                            events:[
-                                // {
-                                //     dataValues: eventOne,
-                                //     // event: "unVgHirSaRI",
-                                //     program: "h0iSBI3xoS6",
-                                //     programStage: "nknoeOj6dLq",
-                                //     orgUnit: row.OrgUIDs,
-                                //     trackedEntityInstance: row.TrackedEntityInstances,
-                                //     trackedEntityType: "T5DWDr5Swce",
-                                //     eventDate: formatDateFromExcelSerial(row.sKrn2rY6l0w),
-                                //     completedDate: "2024-01-27"
-                                // },
-                                {
-                                    dataValues: eventTwo,
-                                    // event: "unVgHirSaRI",
-                                    program: "h0iSBI3xoS6",
-                                    programStage: "s1kg8duJ8U1",
-                                    orgUnit: row.OrgUIDs,
-                                    trackedEntityInstance: row.TrackedEntityInstances,
-                                    trackedEntityType: "T5DWDr5Swce",
-                                    eventDate: formatDateFromExcelSerial(row.eventTwoDate),
-                                    completedDate: "2024-01-27"
-                                }
-                            ]
+                            setJsonData(JSON.stringify(updatedData, null, 2));
+                            console.log("update data", updatedData);
+                            await updateRecord(exists.eventId, event.dataElement, updatedData, updatedCount++);
+                            // updatedCount++;
                         }
 
-                        setJsonData(JSON.stringify(updatedData, null, 2));
-
-                        await updateRecord(id, updatedData, updatedCount);
-                        // updatedCount++;
 
                     } else {
                         // If ID doesn't exist, create payload and post
@@ -161,7 +139,7 @@ function ExcelToJsonConverter() {
 
                         setJsonData(JSON.stringify(convertedJsonData, null, 2));
 
-                        await createRecord(convertedJsonData,createdCount);
+                        // await createRecord(convertedJsonData,createdCount);
                         // createdCount++;
 
                     }
@@ -184,8 +162,12 @@ function ExcelToJsonConverter() {
 
     // Function to check if ID exists at a particular endpoint
     const checkIdExistence = async (id) => {
+        let result = {
+            found: false,
+            eventId: null
+        };
         try {
-            const response = await fetch(`https://uthabitiactivity.org/uthabiti/api/trackedEntityInstances/${id}`,{
+            const response = await fetch(`https://uthabitiactivity.org/uthabiti/api/trackedEntityInstances/${id}?fields=enrollments[events[event,programStage,eventDate,orgUnit]]`,{
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -193,9 +175,36 @@ function ExcelToJsonConverter() {
                 },
             });
 
+
             if (response.status === 200){
                 console.log(`id ${id} found`, response.status)
-                return true
+
+                // Extract JSON data from the response
+                const data = await response.json();
+                const enrollments = data.enrollments
+
+
+                // Looping through enrollments to find the event ID with the specified program stage
+                let eventId = null;
+                for (const enrollment of enrollments) {
+                    const events = enrollment.events;
+                    const filteredEvents = events.filter(event => event.programStage === "s1kg8duJ8U1");
+
+                    if (filteredEvents.length > 1) {
+                        console.log("There are more than one event");
+                        result.found = true;
+                        break; // Exit loop if more than one event is found
+                    } else if (filteredEvents.length === 1) {
+                        eventId = filteredEvents[0].event;
+                        // Proceed to update
+                        console.log("Event ID:", eventId);
+                        result.found = true;
+                        result.eventId = filteredEvents[0].event;
+                        break; // Exit loop if one event is found
+                    }
+                }
+
+                return result
             } else {
                 console.log(`id ${id} not found`, response.status)
                 return false
@@ -208,15 +217,15 @@ function ExcelToJsonConverter() {
     };
 
     // Function to update record at
-    const updateRecord = async (id, data, updatedCount) => {
+    const updateRecord = async (eventId, id, data, updatedCount) => {
 
         //  POST jsonData to an API endpoint
         try {
-            const response = await fetch('https://uthabitiactivity.org/uthabiti/api/events', {
-                method: 'POST',
+            const response = await fetch(`https://uthabitiactivity.org/uthabiti/api/events/${eventId}/${id}`, {
+                method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': 'Basic ' + btoa('Myco:Caf3t3ria!'),
+                    'Authorization': 'Basic ' + btoa('Sci_test:Save1234!'),
                 },
                 body: JSON.stringify(data),
                 credentials: 'include',
